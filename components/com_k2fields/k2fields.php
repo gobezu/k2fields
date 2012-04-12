88027d0c223c464f58fd95db855d2a61eb4cdf34
Gobezu Sewu <gobezu@gmail.com>
2012-04-12 15:47:45 +0300

    Missed loading helper classes in earlier commit where we introduced K2FieldsHelperRoute

--- components/com_k2fields/k2fields.php
+++ components/com_k2fields/k2fields.php
@@ -9,7 +9,9 @@ $task = JRequest::getCmd('task', 'search');
 if ($view == 'fields' || $view == 'field') {
         require_once JPATH_ADMINISTRATOR.'/components/com_k2fields/k2fields.php';
 } else if ($view == 'item' || $view == 'itemlist' && $task == 'search' || $view == 'rate' && $task == 'values') {
-        require_once JPATH_SITE.'/components/com_k2/helpers/permissions.php';
+        JLoader::register('K2HelperRoute', JPATH_SITE.'/components/com_k2/helpers/route.php');
+        JLoader::register('K2HelperPermissions', JPATH_SITE.'/components/com_k2/helpers/permissions.php');
+        JLoader::register('K2HelperUtilities', JPATH_SITE.'/components/com_k2/helpers/utilities.php');
 
         K2HelperPermissions::setPermissions();
         K2HelperPermissions::checkPermissions();
