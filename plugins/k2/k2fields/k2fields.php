a5ca09cdaeff91bea260b30b0f2dfa0699c88c5a
Gobezu Sewu <gobezu@gmail.com>
2012-04-22 11:21:56 +0300

    Merged version of js has now the same version independent name and version tag is attached at inclusion

--- plugins/k2/k2fields/k2fields.php
+++ plugins/k2/k2fields/k2fields.php
@@ -760,10 +760,10 @@ fjs.parentNode.insertBefore(js, fjs);
                         
                         if ($tab == 'menu') JprovenUtility::load('jpmenuitemhandler.js', 'js');
                         
-                        $js = JFolder::files(JPATH_SITE.'/media/k2fields/js/', '^k2fields\.v.+\.js$');
-                        
-                        if (!empty($js)) {
-                                JprovenUtility::load($js[count($js) - 1], 'js');
+                        $js = 'media/k2fields/js/k2fields.all.js';
+                        if (!JFile::exists(JPATH_SITE.'/media/k2fields/js/k2fields.all.js')) {
+                                $ver = "$Ver$";
+                                JprovenUtility::load('k2fields.all.js?v='.$ver, 'js');
                                 $compressedLoaded = true;
                         } else {
                                 JprovenUtility::load('jpform.js', 'js');
